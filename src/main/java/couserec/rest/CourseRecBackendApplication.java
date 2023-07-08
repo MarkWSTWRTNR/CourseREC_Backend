36d1f1f4a078a9453395c26251619534ee0bbeda
markwst <Wongsathorn_W@gmail.com>
2023-07-08 17:00:59 +0700

    connected with DB

--- src/main/java/couserec/rest/CourseRecBackendApplication.java
+++ src/main/java/couserec/rest/CourseRecBackendApplication.java
@@ -2,12 +2,27 @@ package couserec.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CourseRecBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CourseRecBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRecBackendApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "http://18.142.246.86:4000")
+                        .exposedHeaders("x-total-count");
 
+            }
+        };
+
+    }
 }
