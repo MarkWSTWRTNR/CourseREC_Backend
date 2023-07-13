39b2975d11ec14e124610676bee50deb5d07c032
markwst <Wongsathorn_W@gmail.com>
2023-07-13 23:26:05 +0700

    wait for update

--- src/main/java/couserec/rest/config/InitApp.java
+++ src/main/java/couserec/rest/config/InitApp.java
@@ -44,10 +44,10 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         Program p = programRepository.save(Program.builder().name("SE").programId("01").build());
         Program p2 = programRepository.save(Program.builder().name("ani").programId("02").build());
         Faculty f = facultyRepository.save(Faculty.builder().facultyId("01").name("CAMT").build());
-        f.getPrograms().add(p);
-        p.setFaculty(f);
-        f.getPrograms().add(p2);
-        p2.setFaculty(f);
+//        f.getPrograms().add(p);
+//        p.setFaculty(f);
+//        f.getPrograms().add(p2);
+//        p2.setFaculty(f);
 //
 //        b.getPrerequisite().add(a);
 //        Curriculum ccl = curriculumRepository.save(Curriculum.builder().credit(30).name("GELP").build());
--- src/main/java/couserec/rest/entity/ProgramDTO.java
+++ src/main/java/couserec/rest/entity/ProgramDTO.java
@@ -1,4 +1,16 @@
 package couserec.rest.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProgramDTO {
+    private int id;
+    private String programId;
+    private String name;
 }
