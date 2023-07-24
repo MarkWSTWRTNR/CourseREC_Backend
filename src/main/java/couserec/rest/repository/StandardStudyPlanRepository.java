d7233aee27c7caa41602805519eaca783f03b334
markwst <Wongsathorn_W@gmail.com>
2023-07-24 12:53:58 +0700

    fix bug

--- src/main/java/couserec/rest/repository/StandardStudyPlanRepository.java
+++ src/main/java/couserec/rest/repository/StandardStudyPlanRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StandardStudyPlanRepository extends JpaRepository<StandardStudyPlan, Integer> {
-    List<StandardStudyPlan> findAl();
+    List<StandardStudyPlan> findAll();
 }
