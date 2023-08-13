8dfb9f74d4f0acfd3423c8f68d2c0212cded6410
markwst <Wongsathorn_W@gmail.com>
2023-08-13 15:43:42 +0700

    finished course + chatgpt

--- src/main/java/couserec/rest/controller/ChatGPTController.java
+++ src/main/java/couserec/rest/controller/ChatGPTController.java
@@ -2,6 +2,8 @@ package couserec.rest.controller;
 
 import couserec.rest.entity.ChatGPT.dto.ChatGPTRequest;
 import couserec.rest.entity.ChatGPT.dto.ChatGPTResponse;
+import couserec.rest.entity.FinishedCourse;
+import couserec.rest.service.FinishedCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bot")
@@ -20,11 +24,26 @@ public class ChatGPTController {
 
     @Autowired
     private RestTemplate template;
+    @Autowired
+    private FinishedCourseService finishedCourseService;
 
     @GetMapping("/chat")
     public String chat(@RequestParam("prompt") String prompt) {
-        ChatGPTRequest request = new ChatGPTRequest(model,prompt);
-        ChatGPTResponse chatGPTResponse = template.postForObject(apiURL,request, ChatGPTResponse.class);
+        List<FinishedCourse> finishedCourses = finishedCourseService.getStudentFinishedCourse();
+
+        StringBuilder promptBuilder = new StringBuilder("If I have finished these subjects:");
+
+        for (FinishedCourse finishedCourse : finishedCourses) {
+            promptBuilder.append(" ").append(finishedCourse.getName()).append(",");
+        }
+
+        // Remove the last comma and add the rest of the prompt
+        promptBuilder.deleteCharAt(promptBuilder.length() - 1);
+        promptBuilder.append(" ").append(prompt);
+
+        ChatGPTRequest request = new ChatGPTRequest(model, promptBuilder.toString());
+        ChatGPTResponse chatGPTResponse = template.postForObject(apiURL, request, ChatGPTResponse.class);
+
         return chatGPTResponse.getChoices().get(0).getMessage().getContent();
     }
 }
