c1ed4e944dbf9c11d04c6daa8494a8e5639e4ed3
markwst <Wongsathorn_W@gmail.com>
2023-08-22 09:49:35 +0700

    update chat gpt

--- src/main/java/couserec/rest/controller/ChatGPTController.java
+++ src/main/java/couserec/rest/controller/ChatGPTController.java
@@ -7,6 +7,7 @@ import couserec.rest.entity.FinishedCourse;
 import couserec.rest.service.FinishedCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/bot")
@@ -30,15 +33,17 @@ public class ChatGPTController {
     private FinishedCourseService finishedCourseService;
 
     @GetMapping("/chat")
-    public String chat() {
+    public ResponseEntity<Map<String, Object>> chat() {
 
         List<FinishedCourse> finishedCourses = finishedCourseService.getStudentFinishedCourse();
 
         StringBuilder promptBuilder = new StringBuilder("If I have finished these subjects:");
+        List<String> finishedCourseNames = new ArrayList<>();
 
         for (FinishedCourse finishedCourse : finishedCourses) {
             for (Course course : finishedCourse.getCourses()) {
                 promptBuilder.append(" ").append(course.getName()).append(",");
+                finishedCourseNames.add(course.getName());
             }
         }
 
@@ -49,7 +54,11 @@ public class ChatGPTController {
         ChatGPTRequest request = new ChatGPTRequest(model, promptBuilder.toString());
         ChatGPTResponse chatGPTResponse = template.postForObject(apiURL, request, ChatGPTResponse.class);
 
-        return chatGPTResponse.getChoices().get(0).getMessage().getContent();
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("finishedCourses", finishedCourseNames); // This will show the course names sent to ChatGPT
+        responseMap.put("recommendation", chatGPTResponse.getChoices().get(0).getMessage().getContent());
+
+        return ResponseEntity.ok(responseMap);
     }
 }
 
